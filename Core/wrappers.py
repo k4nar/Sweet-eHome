edd18942a1c9e2afe15b41e016ce99dbbb571d97
Yannick PEROUX <yannick.peroux@gmail.com>
2013-02-25 11:12:58 +0100

    Creation of get_all_wrappers

--- Core/wrappers.py
+++ Core/wrappers.py
@@ -4,17 +4,19 @@ libs = {
     "DumbDevices": 'DumbDevicesLib',
 }
 
+def get_all_wrappers(wrappers):
+    for name, module_name in libs:
+        try:
+            module = __import__(module_name)
+
+            wrappers[name] = {
+                "listener": module.Listener(),
+                "sender": module.Sender(),
+                "broadcaster": module.Broadcaster()
+            }
+            
+        except:
+            pass
+
 wrappers = {}
-
-for name, module_name in libs:
-    try:
-        module = __import__(module_name)
-
-        wrappers[name] = {
-            "listener": module.Listener(),
-            "sender": module.Sender(),
-            "broadcaster": module.Broadcaster()
-        }
-        
-    except:
-        pass
+get_all_wrappers(wrappers)
