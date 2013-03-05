83e1f1ba03d15bfc179718bf472c6d70b0cb1864
Yannick PEROUX <yannick.peroux@gmail.com>
2013-03-05 01:51:28 +0100

    Mooving ZwaveLib to it's own package

--- ZwaveLib/listener.py
+++ ZwaveLib/listener.py
@@ -1,20 +1,23 @@
-from listener import BaseListener
+from DomoLib import BaseListener
 
 import openzwave
 from openzwave import PyManager
 
+
 class Listener(BaseListener):
-    def __init__(self):
-        super(Listener, self).__init__()
+    def __init__(self, core):
+        super(Listener, self).__init__(core)
 
         self.options = openzwave.PyOptions()
-        self.options.create("openzwave/config/","","")
+        self.options.create("openzwave/config/", "", "")
         self.options.lock()
+        
         self.manager = openzwave.PyManager()
         self.manager.create()
 
         self.manager.addWatcher(self.receive)
-        self.manager.addDriver('/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0')
+        self.manager.addDriver(
+            '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0')
 
     def handler(self, args):
         print args
